bedce9793ea1e0590f89be73126ece681d73a5f7
themuffinator <djdacosta86@yahoo.co.uk>
2025-10-14 18:30:16 +0100

    Preserve swapchain image contents for overlay pass

--- src/renderer_vk/device.cpp
+++ src/renderer_vk/device.cpp
@@ -523,7 +523,7 @@ bool VulkanRenderer::createSwapchainResources(VkSwapchainKHR oldSwapchain) {
     VkAttachmentDescription colorAttachment{};
     colorAttachment.format = swapchainFormat_;
     colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
-    colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_LOAD;
     colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
     colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
     colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
