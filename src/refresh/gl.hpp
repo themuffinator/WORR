df52b880c762e9c0ec6bdeee54a66e37710b9d81
themuffinator <djdacosta86@yahoo.co.uk>
2025-11-02 18:18:58 +0000

    Simplify underwater warp toggles

--- src/refresh/gl.hpp
+++ src/refresh/gl.hpp
@@ -398,7 +398,7 @@ extern cvar_t *gl_md5_use;
 extern cvar_t *gl_md5_distance;
 #endif
 extern cvar_t *gl_damageblend_frac;
-extern cvar_t *gl_waterwarp;
+extern cvar_t *r_skipUnderWaterFX;
 extern cvar_t *gl_bloom;
 extern cvar_t *gl_bloom_height;
 extern cvar_t *gl_dof;
