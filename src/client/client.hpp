af86a3a77dab5061f0f2031ce60f2c8f2c93a0ef
themuffinator <djdacosta86@yahoo.co.uk>
2025-11-02 13:57:18 +0000

    Fix client build regressions

--- src/client/client.hpp
+++ src/client/client.hpp
@@ -47,11 +47,10 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "common/sizebuf.hpp"
 #include "common/zone.hpp"
 
+#include "client/client.hpp"
 #include "refresh/refresh.hpp"
 #include "server/server.hpp"
 #include "system/system.hpp"
-
-#include "client/client.hpp"
 #include "client/input.hpp"
 #include "client/keys.hpp"
 #include "client/sound/sound.hpp"
